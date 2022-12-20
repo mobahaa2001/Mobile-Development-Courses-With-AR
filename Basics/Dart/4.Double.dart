e061479b0b6641d184c93ec74351b66c862a6fce
Mego <70282836+mobahaa2001@users.noreply.github.com>
2022-12-21 00:50:46 +0200

    Add files via upload

---
+++ Basics/Dart/4.Double.dart
@@ -0,0 +1,28 @@
+void main()
+{
+  // Double
+  // هي نوع مِن أنواع الـ Data Type ولكن تستخدم مع الأرقام مِثل int والإختلاف بين int and double هو أنِّ
+  // تستخدم double للأرقام العشرية
+  // بينَمَا تستخدم int للأرقام الصحيحة
+
+  double x= 1.2;
+  double y= 3.4;
+  // لو أردنا أنْ نقزم بجمع الرقمين فأكيد سنحصل على 4.6 فيجب علينا أنْ نستخدم في حالة جمعهم double
+
+  double z = x+y;
+  print(z);
+
+  // ملحوظة : إذا كتبت أو سجلت رقم صحيح في الـ double فسيسجل بكسر عشري
+  // مثال
+
+  double k = 5;
+  print(k);
+  // هنا سيظهر الناتِج 5.0 وليس 5
+
+  // double + double = double
+  // int + int = int
+  // int + double = double
+  // double + int = double
+
+
+}
