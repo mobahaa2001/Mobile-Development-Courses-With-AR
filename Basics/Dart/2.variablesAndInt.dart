a4005f61742efb057f5ef39de3c0a1e17963e97c
Mego <70282836+mobahaa2001@users.noreply.github.com>
2022-12-20 20:02:23 +0200

    Add files via upload

---
+++ Basics/Dart/2.variablesAndInt.dart
@@ -0,0 +1,20 @@
+void main()
+{
+  /*
+    تتكون الجملة أو سطر الأوامِر مِن جزئين
+    1- Data Type
+    2- Name (Address)
+    3- Value
+  */
+  // DataType Name = DataValue;
+  int x = 10;
+  // int is Data Type
+  // x is Name
+  // 10 is Value
+  print(x);
+  // هِنا تم تخزين بقيمة معينة لـ x
+  x=50;
+  // دلوقتي x أتغيرت قيمتها لـ 50
+  print(x);
+
+}
