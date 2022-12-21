7d2c885901987bbe6786272065ad35526ebdde12
Mego <70282836+mobahaa2001@users.noreply.github.com>
2022-12-22 01:42:03 +0200

    Add files via upload

---
+++ Basics/Dart/12.RelationalOperators.dart
@@ -0,0 +1,40 @@
+void main()
+{
+  // Relational Operators
+  // هو للسأل غن أكبر مِن أصغر مِن.... إلخ
+
+  // > Greater than
+  // < Lesser than
+  // >= Greater than or equal
+  // <= Lesser than or equal
+  // == equality
+  // != not equal
+
+  int x = 10;
+  int y = 8;
+
+  bool z = x < y;
+  print(z);
+  // false
+
+  z = x > y;
+  print(z);
+  // true
+
+  z = x == y;
+  print(z);
+  // false
+
+  z = x >= y;
+  print(z);
+  // true
+
+  z = x<= y;
+  print(z);
+  // false
+
+  z = x != y;
+  print(z);
+  // true
+
+}
