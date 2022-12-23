426b86b135b4117e49b91fd737643e310e3c43f9
Mego <70282836+mobahaa2001@users.noreply.github.com>
2022-12-23 02:11:26 +0200

    Add files via upload

---
+++ Basics/Dart/20.SwitchCse.dart
@@ -0,0 +1,31 @@
+void main()
+{
+  // Switch Case
+  // هي طريقة شبية لحالة if
+
+  // Football : 1
+  // Basketball : 2
+  // Tennis : 3
+
+  var x=2;
+  String y;
+
+
+  switch(x){
+    case 1 :
+      y="Football";
+      break;
+    case 2 :
+      y="Basketball";
+      break;
+    case 3 :
+      y="Tennis";
+      break;
+    default:
+      y="U Don't have favorite";
+
+  }
+  print(y);
+  // Basketball
+  // أنتَ هنا بتقوله لمَّا يكون إختياره x كذا يكون الإجابة بتاعته كذا
+}
