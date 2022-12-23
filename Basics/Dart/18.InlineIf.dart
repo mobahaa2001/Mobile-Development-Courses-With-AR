61d860009f7af22c74fb330537aaf552ff96ccb0
Mego <70282836+mobahaa2001@users.noreply.github.com>
2022-12-23 02:10:56 +0200

    Add files via upload

---
+++ Basics/Dart/18.InlineIf.dart
@@ -0,0 +1,17 @@
+void main()
+{
+  // Inline If
+  // condition ? if true : if else
+  // condition ? code : code
+  // يعني بنسأل لو صح تمام لو مش صح هيأخذ السباب التاني
+
+  int a = 8;
+  int b = -7;
+
+  String c = a < b ? 'a Lesser Than b' : 'a Greater Than b';
+  print(c);
+  // a Greater Than b
+  // هذا يدل على أنَّ a أكبر مِن b وهذه المعادلة خطأة
+  
+
+}
