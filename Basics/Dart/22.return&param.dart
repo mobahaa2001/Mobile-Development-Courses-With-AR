8c5b9a48ddb8e8b294d30f47c7e5255d9a6fda6e
Mego <70282836+mobahaa2001@users.noreply.github.com>
2022-12-24 02:38:06 +0200

    Add files via upload

---
+++ Basics/Dart/22.return&param.dart
@@ -0,0 +1,19 @@
+void main()
+{
+  // Return and Params
+
+  print(addition(5, 8));
+  // 13
+
+
+  print(addition(80, -9));
+  // 71
+
+  // استخدمنا return لإستخدام القيمة و params لإدخال عدد أو قيمة في العملية الحسابية
+}
+
+
+int addition(x,y)
+{
+  return x+y;
+}
