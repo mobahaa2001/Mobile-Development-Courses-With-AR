a8a85746db86b5bc7c247b7d030352821e433dfd
Mego <70282836+mobahaa2001@users.noreply.github.com>
2022-12-24 02:28:28 +0200

    Add files via upload

---
+++ Basics/Dart/21.Functions.dart
@@ -0,0 +1,18 @@
+void main()
+{
+  // Functions or Methods
+  // هي مساحة مغلقة ذات اسم يدخل فيهِ شيء ويخرج مِنهُ شيء أخر
+  // يتكون مِن مساحة بأسمَه فندخل شيء(param) ويخرج مِنْهُ قيمة (return)
+  // return.type Name (params){___________________________
+  // _______________________________
+  // _____________________________
+  // }
+
+  // call method (function)
+  fullName();
+  // Mego from out
+}
+
+void fullName() {
+  print("Mego from out");
+}
