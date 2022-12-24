0654d39c61290ecbdcfb204e223e492dfa2c79dc
Mego <70282836+mobahaa2001@users.noreply.github.com>
2022-12-24 05:21:10 +0200

    Add files via upload

---
+++ Basics/Dart/23.ArrowFunction.dart
@@ -0,0 +1,12 @@
+void main()
+{
+  // Arrow Function
+  // هي نوع مِن الـ Function ولكنَّها في سطرٍ واحد
+
+  print(sum(2, 8));
+  print(sum(12, 81));
+  print(sum(8, -7));
+
+}
+
+int sum(a,b) => a+b;
