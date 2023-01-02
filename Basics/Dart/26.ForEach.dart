d1baddaf3977288b304e8c7c92b3073d7f950d1f
Mego <70282836+mobahaa2001@users.noreply.github.com>
2023-01-02 05:24:47 +0200

    Add files via upload

---
+++ Basics/Dart/26.ForEach.dart
@@ -0,0 +1,14 @@
+void main()
+{
+  // forEach
+  // هي method وديه بتلوب على كل القيم اللي في الليسته Array
+
+List<String> myFriends = ["Muhammed", "Bahaa", "Abdelrahman", "Ahmad", "Salem"];
+
+myFriends.forEach((element) {print(element);});
+//Muhammed
+// Bahaa
+// Abdelrahman
+// Ahmad
+// Salem
+}
